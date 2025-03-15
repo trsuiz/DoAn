c7a62c078d217e8d1b8203a9100e03b103f6436b
Trần Trọng Khang <23DH111538@st.huflit.edu.vn>
2025-03-15 23:08:32 +0700

    //BimyKhoa TAU HAI

--- app/src/main/java/com/example/doan/Family.java
+++ app/src/main/java/com/example/doan/Family.java
@@ -20,5 +20,5 @@ public class Family extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+    } // BimmyKhoa Tau HAI
 }
