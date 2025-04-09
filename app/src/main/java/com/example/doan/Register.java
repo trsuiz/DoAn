5bf31df535373635700b207be5f1cbc3f37478bf
Kha004 <vanhoc463@gmail.com>
2025-04-09 10:38:25 +0700

    ẩn hiện password,confirm password (register)

--- app/src/main/java/com/example/doan/Register.java
+++ app/src/main/java/com/example/doan/Register.java
@@ -2,9 +2,11 @@ package com.example.doan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -23,7 +25,9 @@ public class Register extends AppCompatActivity {
     private EditText confirmPasswordInput;
     private Button registerBtn;
     private FirebaseAuth mAuth;
-
+    private ImageView passwordToggle, confirmPasswordToggle;
+    private boolean isPasswordVisible = false;
+    private boolean isConfirmPasswordVisible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +45,31 @@ public class Register extends AppCompatActivity {
         fullNameInput = findViewById(R.id.fullname_input);
         confirmPasswordInput = findViewById(R.id.confirm_password_input);
         registerBtn = findViewById(R.id.register_btn);
-
+        confirmPasswordInput = findViewById(R.id.confirm_password_input);
+        passwordToggle = findViewById(R.id.password_toggle);
+        confirmPasswordToggle = findViewById(R.id.confirm_password_toggle);
         registerBtn.setOnClickListener(v -> registerUser());
+
+        passwordToggle.setOnClickListener(v -> {
+            isPasswordVisible = !isPasswordVisible;
+            passwordInput.setInputType(isPasswordVisible ?
+                    InputType.TYPE_CLASS_TEXT :
+                    InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            passwordInput.setSelection(passwordInput.length());
+            passwordToggle.setImageResource(isPasswordVisible ?
+                    R.drawable.ic_eye_open : R.drawable.ic_eye_closed);
+        });
+
+        confirmPasswordToggle.setOnClickListener(v -> {
+            isConfirmPasswordVisible = !isConfirmPasswordVisible;
+            confirmPasswordInput.setInputType(isConfirmPasswordVisible ?
+                    InputType.TYPE_CLASS_TEXT :
+                    InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            confirmPasswordInput.setSelection(confirmPasswordInput.length());
+            confirmPasswordToggle.setImageResource(isConfirmPasswordVisible ?
+                    R.drawable.ic_eye_open : R.drawable.ic_eye_closed);
+        });
+
     }
 
     private void registerUser() {
